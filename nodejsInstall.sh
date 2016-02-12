634364d8b42473be8844eec2d4dfde39444b199c
gyuha <gyuha@talklish.com>
2016-02-12 14:53:51 +0900

    node install script add

---
+++ nodejsInstall.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+if [ $UID -ne 0 ]; then
+	echo Non root user. Please run as root.
+	exit 1;
+fi
+
+VERSION=5
+if [ -n "$1" ]; then
+	VERSION=$1
+fi
+
+re='^[0-9]+$'
+if ! [[ $VERSION =~ $re ]]; then
+	echo "Version is number"
+	exit 1
+fi
+
+apt-get remove --purge nodejs
+echo Install : https://deb.nodesource.com/setup_$VERSION.x
+curl -sL https://deb.nodesource.com/setup_$VERSION.x | bash -s
+apt-get install -y nodejs
+apt-get install -y build-essential
