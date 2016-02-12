5c6d83a4ac6e433c720699214e57dbd5441386dd
gyuha <gyuha@talklish.com>
2016-02-12 16:29:55 +0900

    install files update

--- bootstrap.sh
+++ bootstrap.sh
@@ -155,9 +155,6 @@ setup_vundle() {
 }
 
 ############################ MAIN()
-apt-get update
-apt-get install -y vim git
-
 program_exists "vim" "To install $app_name you first need to install Vim."
 
 clone_repo "Successfully cloned $app_name"
