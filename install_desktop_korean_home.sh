285d8a0a3fa7ce2dab78637424cb4dfd9a4d08fb
gyuha <nicegyuha@gmail.com>
2016-02-13 21:16:12 +0900

    update

---
+++ install_desktop_korean_home.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# 한글로 설치 후 기본 폴더명 변경.
+cp -rf ~/.settings/conf/user-dirs.dirs  ~/.config/
+mv -f ~/바탕화면 ~/Desktop
+mv -f ~/다운로드 ~/Downloads
+mv -f ~/템플릿 ~/Templates
+mv -f ~/공개 ~/Public
+mv -f ~/문서 ~/Documents
+mv -f ~/음악 ~/Music
+mv -f ~/사진 ~/Pictures
+mv -f ~/비디오 ~/Videos
