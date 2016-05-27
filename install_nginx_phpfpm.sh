91b560f3238ab6b035eb2a1c0aadb219219f74bc
gyuha <nicegyuha@gmail.com>
2016-05-27 11:04:13 +0900

    php7용 설정 변경

--- install_nginx_phpfpm.sh
+++ install_nginx_phpfpm.sh
@@ -10,22 +10,40 @@ nginx() {
 
 # PHP-FPM install
 phpfpm() {
-	apt_add php7.0 php7.0-fpm php7.0-cli php7.0-common php7.0-curl php7.0-gd php7.0-gmp php7.0-json php7.0-ldap php7.0-mysql php7.0-odbc php7.0-pspell php7.0-readline php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xml php7.0-xmlrpc php7.0-bcmath php7.0-bz2 php7.0-enchant php7.0-fpm php7.0-imap php7.0-interbase php7.0-intl php7.0-mbstring php7.0-mcrypt php7.0-phpdbg php7.0-soap php7.0-sybase php7.0-xsl php7.0-zip php-imagick php-redis php-apc php-pear
+	apt_add php7.0 php7.0-fpm php7.0-cli php7.0-common php7.0-curl php7.0-gd php7.0-gmp php7.0-json php7.0-ldap php7.0-mysql php7.0-odbc php7.0-pspell php7.0-readline php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xml php7.0-xmlrpc php7.0-bcmath php7.0-bz2 php7.0-enchant php7.0-fpm php7.0-imap php7.0-interbase php7.0-intl php7.0-mbstring php7.0-mcrypt php7.0-phpdbg php7.0-soap php7.0-sybase php7.0-xsl php7.0-zip php-imagick php-redis php-apcu php-pear
 }
 
-copyconf() {
-	msg "Copy service conf"
+restart_service() {
+	service php7.0-fpm restart
+	service nginx restart
+}
+
+copy_dev_conf() {
+	msg "Copy development configure"
+	msg "Nginx"
+	copy_increase_number /etc/nginx/sites-available/default /tmp/nginx.conf
+	cp -f ./conf/php7.0/development/nginx.conf /etc/nginx/sites-available/default
+
+	msg "php-fpm"
+	copy_increase_number /etc/php/7.0/fpm/php.ini /tmp/php.ini
+	cp -f ./conf/php7.0/development/php.ini /etc/php/7.0/fpm/php.ini
+
+	restart_service;
+	success "Copy complete"
+}
+
+copy_product_conf() {
+	msg "Copy product configure"
 
 	msg "Nginx"
 	copy_increase_number /etc/nginx/sites-available/default /tmp/nginx.conf
-	cp -f ./conf/nginx.conf /etc/nginx/sites-available/default
+	cp -f ./conf/php7.0/product/nginx.conf /etc/nginx/sites-available/default
 
 	msg "php-fpm"
-	copy_increase_number /etc/php5/fpm/php.ini /tmp/php.dev.ini
-	cp -f ./conf/php5/php.dev.ini /etc/php5/fpm/php.ini
+	copy_increase_number /etc/php/7.0/fpm/php.ini /tmp/php.ini
+	cp -f ./conf/php7.0/product/php.ini /etc/php/7.0/fpm/php.ini
 
-	service php5-fpm restart
-	service nginx restart
+	restart_service;
 	success "Copy complete"
 }
 
@@ -37,9 +55,13 @@ if [ $PACKAGES == "all" ]; then
 	exit;
 fi
 
-if [ $1 == "copyconf" ]; then
-	copyconf;
+if [ $1 == "dev" ]; then
+	copy_dev_conf;
+	exit;
+elif [ $1 == "product" ]; then
+	copy_product_conf;
 	exit;
 fi
+
 
 source ./install_run.sh
