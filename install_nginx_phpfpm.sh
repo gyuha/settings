c07a56d56e6ebcccc4c5a54d0bc25764ce787fa8
gyuha <nicegyuha@gmail.com>
2016-05-30 11:28:11 +0900

    add mongodb install

--- install_nginx_phpfpm.sh
+++ install_nginx_phpfpm.sh
@@ -10,7 +10,7 @@ nginx() {
 
 # PHP-FPM install
 phpfpm() {
-	apt_add php7.0 php7.0-fpm php7.0-cli php7.0-common php7.0-curl php7.0-gd php7.0-gmp php7.0-json php7.0-ldap php7.0-mysql php7.0-odbc php7.0-pspell php7.0-readline php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xml php7.0-xmlrpc php7.0-bcmath php7.0-bz2 php7.0-enchant php7.0-fpm php7.0-imap php7.0-interbase php7.0-intl php7.0-mbstring php7.0-mcrypt php7.0-phpdbg php7.0-soap php7.0-sybase php7.0-xsl php7.0-zip php-imagick php-redis php-apcu php-pear
+	apt_add php7.0 php7.0-fpm php7.0-cli php7.0-common php7.0-curl php7.0-gd php7.0-gmp php7.0-json php7.0-ldap php7.0-mysql php7.0-odbc php7.0-pspell php7.0-readline php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xml php7.0-xmlrpc php7.0-bcmath php7.0-bz2 php7.0-enchant php7.0-fpm php7.0-imap php7.0-interbase php7.0-intl php7.0-mbstring php7.0-mcrypt php7.0-phpdbg php7.0-soap php7.0-sybase php7.0-xsl php7.0-zip php-imagick php-redis php-apcu php-pear php-mongodb
 }
 
 restart_service() {
