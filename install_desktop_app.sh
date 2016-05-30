a11b3c31138daf3273950d0a68032ee6b10462c3
gyuha <nicegyuha@gmail.com>
2016-05-30 11:23:36 +0900

    korean home move

--- install_desktop_app.sh
+++ install_desktop_app.sh
@@ -9,6 +9,19 @@ copyconf() {
 	success "Complete"
 }
 
+koreanHome() {
+	# 한글로 설치 후 기본 폴더명 변경.
+	cp -rf ~/.settings/conf/user-dirs.dirs  ~/.config/
+	mv -f ~/바탕화면 ~/Desktop
+	mv -f ~/다운로드 ~/Downloads
+	mv -f ~/템플릿 ~/Templates
+	mv -f ~/공개 ~/Public
+	mv -f ~/문서 ~/Documents
+	mv -f ~/음악 ~/Music
+	mv -f ~/사진 ~/Pictures
+	mv -f ~/비디오 ~/Videos
+}
+
 disableUnnecessayErrorMessage() {
 	msg "Disable Unnecessary Error Messages from Appearing"
     sed -i "s/enabled=1/enabled=0/" /etc/default/apport
