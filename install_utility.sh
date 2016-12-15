8d51b17b9f1b8de99edca548a5af3554597dfd51
gyuha <nicegyuha@gmail.com>
2016-12-15 16:00:48 +0900

    mongodb install add

--- install_utility.sh
+++ install_utility.sh
@@ -23,6 +23,13 @@ mariadb() {
 	apt_add mariadb-server mariadb-client
 }
 
+# mongodb install
+mongodb() {
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+	echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+	apt_add mongodb-org
+}
+
 
 if [ $PACKAGES == "all" ]; then
 	msg "Install all packages."
