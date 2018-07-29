9d788a5129d91f5a5f29eaf39776def167dde0ef
gyuha <nicegyuha@gmail.com>
2018-07-29 21:59:32 +0900

    add docker install

--- install_utility.sh
+++ install_utility.sh
@@ -23,6 +23,11 @@ mariadb() {
 	apt_add mariadb-server mariadb-client
 }
 
+# Docker 
+docker() {
+	apt_add docker.io
+}
+
 # mongodb install
 mongodb() {
 	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
