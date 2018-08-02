52eb5d73b5e6ddd6cba36de2c15dc4b7eed68ebf
gyuha <nicegyuha@gmail.com>
2018-08-02 11:05:51 +0900

    docker로 mariadb 설치하는거 추가

---
+++ docker_mariadb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ $UID -ne 0 ]; then
+	echo Non root user. Please run as root.
+	exit 1;
+fi
+
+function usage()
+{
+	echo "Install MariaDB by docker
+Usage: `basename $0` [-h] ROOT_PASSWORD
+	-h : help
+	ROOT_PASSWORD : database root account password"
+}
+
+case "$1" in
+	"-h")
+		usage;
+		exit 0;
+		;;
+esac
+
+# 만약 파라메터 2개가 아니면 종료
+[ $# -lt 1 ] && usage && exit 1
+
+docker pull mariadb:latest
+docker run -p 3306:3306 --name mariadb -e MYSQL_ROOT_PASSWORD=$1 -d mysql:latest
