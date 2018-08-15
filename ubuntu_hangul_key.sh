05196e0ca8c641d998586c326403122179a37412
gyuha <nicegyuha@gmail.com>
2018-08-15 10:33:13 +0900

    update

--- ubuntu_hangul_key.sh
+++ ubuntu_hangul_key.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# 우분투에서 한글키 매핑해 주기
+# 참고 : http://progtrend.blogspot.com/2018/06/ubuntu-1804-uim.html
+
 #오른쪽 Alt키의 기본 키 맵핑을 제거하고 'Hangul'키로 맵핑
 xmodmap -e 'remove mod1 = Alt_R'
 xmodmap -e 'keycode 108 = Hangul'
