17f2a3533aacda072e40655a60491d4a9d9f70f6
gyuha <nicegyuha@gmail.com>
2018-09-15 22:52:21 +0900

    add mpv config

--- app_config.sh
+++ app_config.sh
@@ -1,23 +1,37 @@
 #!/usr/bin/env bash
 
+MAGENTA='\e[0;35m'
+END_COLOR='\e[0m'
+
+msg() {
+	printf $MAGENTA'%b\n'$END_COLOR "$1" >&2
+}
+
 geany() {
-	echo  "Copy Geany settings"
+	msg  "Copy Geany settings"
 	cp -rf ./conf/geany/colorschemes ~/.config/geany
-	echo  "Complete"
+	echo "Complete"
 }
 
 doublecmd() {
-	echo  "Copy Double Commander settings"
+	msg  "Copy Double Commander settings"
 	cp -rf ./conf/doublecmd ~/.config
 	echo  "Complete"
 }
 
 mcomix() {
-	echo  "Copy MComix settings"
+	msg  "Copy MComix settings"
 	cp -rf ./conf/mcomix ~/snap/mcomix-tabetai/current/.config/
+	echo  "Complete"
+}
+
+mpv() {
+	msg  "Copy MComix settings"
+	cp -rf ./conf/mpv ~/.config
 	echo  "Complete"
 }
 
 geany;
 doublecmd;
 mcomix;
+mpv;
