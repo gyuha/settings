391c72a9cb81cd76e0bdbe026f8bf6cb66bbf405
gyuha <nicegyuha@gmail.com>
2019-04-11 01:36:53 +0900

    update

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,33 +1,6 @@
 #!/bin/bash
-# sudo apt-get install zsh
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# chsh -s `which zsh`
+sudo apt-get install -f zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s `which zsh`
 
-SCRIPT=$(readlink -f "$0")
-BASEDIR=$(dirname "$SCRIPT")
-PROFILE=$HOME"/.zshrc"
-BASHRC_SRC="# GYUHA SETTINGS
-if [ -f $BASEDIR/conf/sh_profile ]; then
-    . $BASEDIR/conf/sh_profile
-fi
-"
-
-# 테마 설정하기
-sed -i "s/^ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/" $PROFILE
-
-# 개인 설정 적용하기
-LINE=`grep -n "# GYUHA" $PROFILE |sed 's/\:.*$//g'`
-
-if [ "$LINE" == "" ]
-then
-    echo "$BASHRC_SRC" >> $PROFILE
-else
-	LINE=`expr $LINE - 1`
-	echo "Exist profile settings"
-	TMP_FILE="tmp"
-	head -n $LINE $PROFILE > $TMP_FILE
-	echo "$BASHRC_SRC" >> $TMP_FILE
-	mv -f $TMP_FILE $PROFILE
-fi
-
-echo -e "Type this \\n\\t# source $PROFILE"
+./zsh_setup.sh
