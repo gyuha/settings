8c8b0eb4481c0ff17c8ddff8bac88050a50c5df8
gyuha <nicegyuha@gmail.com>
2019-08-11 01:54:00 +0900

    update

--- applications/docker.sh
+++ applications/docker.sh
@@ -5,8 +5,9 @@ if [ $UID -ne 0 ]; then
 fi
 
 apt update
-apt install apt-transport-https ca-certificates curl software-properties-common
+apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt update
 apt-cache policy docker-ce
+apt -y install docker-ce
