06ef713373ded217bd283341f96c84a823b72a3b
gyuha <nicegyuha@gmail.com>
2019-08-13 17:26:46 +0900

    add docker install mysql

---
+++ docker/mysql.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#if [ $UID -ne 0 ]; then
+	#echo Non root user. Please run as root.
+	#exit 1;
+#fi
+
+VOLUME_NAME=mysql.data
+
+function usage()
+{
+	echo "Install mysql by docker
+Usage: `basename $0` [-h] ROOT_PASSWORD
+	-h : help
+	ROOT_PASSWORD : database root account password"
+}
+
+# 기본 msyql 만들기
+function createVolume()
+{
+	dbexist=$(docker volume ls|awk '{print $2}'|grep $VOLUME_NAME)
+	if [ ! -n "$dbexist" ]; then
+		echo "Create mongodb voume"
+		docker volume create $VOLUME_NAME
+	fi
+}
+
+case "$1" in
+	"-h")
+		usage;
+		exit 0;
+		;;
+esac
+
+echo -n "Input MySQL Password : "
+read -s password
+echo
+
+#echo -n "Input Database volume path : "
+#read path
+#echo $path
+
+
+#volume=""
+#if [ -n "$path" ]; then
+	#mkdir -p $path
+
+	#if [ ! -d $path ]; then
+		#echo "Can not make path : $path"
+		#exit
+	#fi
+	#volume=" --volume $path:/var/lib/mysql "
+#fi
+
+createVolume;
+
+docker run -p 3306:3306 --restart=always \
+	--volume $VOLUME_NAME:/var/lib/mysql \
+	--name mysql \
+	-e MYSQL_ROOT_PASSWORD=$password \
+	-d mysql:5.7
