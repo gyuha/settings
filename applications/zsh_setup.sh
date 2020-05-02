70c2b268f3ad31d14d71e0be009aeeb6cd619bc8
gyuha <nicegyuha@gmail.com>
2020-05-02 13:51:42 +0900

    zsh 설정 업데이트

--- applications/zsh_setup.sh
+++ applications/zsh_setup.sh
@@ -13,9 +13,23 @@ fi
 wget http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme && mv bullet-train.zsh-theme $ZSH_CUSTOM/themes/
 
 # 테마 설정하기
-sed -i "s/^ZSH_THEME=\".*$"/ZSH_THEME=\"bullet-train\"/" $PROFILE
+sed -i "s/^ZSH_THEME=\".*\"$/ZSH_THEME=\"bullet-train\"/" $PROFILE
 
 # 플러그인 설치
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions --depth=1
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting --depth=1
+git clone https://github.com/lukechilds/zsh-better-npm-completion ~/.oh-my-zsh/custom/plugins/zsh-better-npm-completion --depth=1
+
+# 플로그인 붙여 넣기
+PLUGIN="
+plugins=(
+  git
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+  zsh-better-npm-completion
+)
+"
+echo "$PLUGIN" >> $PROFILE
 
 
 # 개인 설정 적용하기
@@ -25,12 +39,12 @@ if [ "$LINE" == "" ]
 then
     echo "$BASHRC_SRC" >> $PROFILE
 else
-	LINE=`expr $LINE - 1`
-	echo "Exist profile settings"
-	TMP_FILE="tmp"
-	head -n $LINE $PROFILE > $TMP_FILE
-	echo "$BASHRC_SRC" >> $TMP_FILE
-	mv -f $TMP_FILE $PROFILE
+    LINE=`expr $LINE - 1`
+    echo "Exist profile settings"
+    TMP_FILE="tmp"
+    head -n $LINE $PROFILE > $TMP_FILE
+    echo "$BASHRC_SRC" >> $TMP_FILE
+    mv -f $TMP_FILE $PROFILE
 fi
 
 echo -e "Type this \\n\\t# source $PROFILE"
