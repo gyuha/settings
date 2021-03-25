dce5ef9482afb1943f0c692f6c8b447bbcbc617c
gyuha <nicegyuha@gmail.com>
2021-03-25 16:42:03 +0900

    update zsh

--- applications/zsh.sh
+++ applications/zsh.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo apt-get install -f zsh
 sudo apt-get install -f zsh-syntax-highlighting
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+./prezto.sh
 chsh -s `which zsh`
 
 ./zsh_setup.sh
