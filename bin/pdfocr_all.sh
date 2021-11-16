a3225b973e80d95a7ce187b05ea8740b7762c08c
gyuha <nicegyuha@gmail.com>
2021-11-16 23:53:10 +0900

    Update

--- bin/pdfocr_all.sh
+++ bin/pdfocr_all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# 먼저 아래 프로그램을 설치 해야 한다.
+#  - https://github.com/jbarlow83/OCRmyPDF
 echo $1
 #ocrmypdf -l eng+kor "$1"  "$1"
 for f in *.pdf; do
 	echo "#############################################"
 	echo "OCR : $f"
-	ocrmypdf -l eng+kor --pdfa-image-compression auto  "./$f"  "./$f"
+	ocrmypdf -l eng+kor -v 0 -O 1 --skip-text "./$f"  "./$f"
 	echo "################## DONE #####################"
 done
