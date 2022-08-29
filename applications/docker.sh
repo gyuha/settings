dfc05858eff226c48f510d8a28e0f0e30782af8f
gyuha <nicegyuha@gmail.com>
2022-08-29 11:11:49 +0900

    docker install script update

--- applications/docker.sh
+++ applications/docker.sh
@@ -4,13 +4,27 @@
 	#exit 1;
 #fi
 
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt-cache policy docker-ce
-sudo apt -y install docker-ce
+#/bin/bash
+
+# 1. Required dependencies
+sudo apt-get update
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release
+
+# 2. GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# 3. Use stable repository for Docker
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# 4. Install Docker
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+# 5. Add user to docker group
+sudo groupadd docker
+sudo usermod -aG docker $USER
 
 # ctop
 #  - https://github.com/bcicen/ctop
@@ -21,9 +35,30 @@ sudo chmod +x /usr/local/bin/ctop
 #  - https://github.com/jesseduffield/lazydocker
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 
-# docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.25.0-rc2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
-sudo chmod +x /usr/local/bin/docker-compose
+##########################
+# Old script
 
-sudo usermod -aG docker $USER
+#sudo apt update
+#sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+#sudo apt update
+#sudo apt-cache policy docker-ce
+#sudo apt -y install docker-ce
+
+## ctop
+##  - https://github.com/bcicen/ctop
+#sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.3/ctop-0.7.3-linux-amd64 -O /usr/local/bin/ctop
+#sudo chmod +x /usr/local/bin/ctop
+
+## lazydocker
+##  - https://github.com/jesseduffield/lazydocker
+#curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+
+## docker-compose
+#sudo curl -L https://github.com/docker/compose/releases/download/1.25.0-rc2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+#sudo chmod +x /usr/local/bin/docker-compose
+
+#sudo usermod -aG docker $USER
