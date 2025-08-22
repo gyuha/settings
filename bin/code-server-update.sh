6f8cf7e79ad4e4e3304407dccc583d4d159b8365
gyuha <nicegyuha@gmail.com>
2025-08-22 22:04:25 +0900

    update

--- bin/code-server-update.sh
+++ bin/code-server-update.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 curl -fsSL https://code-server.dev/install.sh | sh
+
+# 폰트 업데이트
+# lib/vscode/out/vs/code/browser/workbench/workbench.html
+#
+# 파일에 아래 내용을 추가 해 줘야 함.
+#  <link rel="stylesheet" href="/fonts/fonts.css">
--- bin/os_version.sh
+++ bin/os_version.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# OS 버전 정보 출력 스크립트
+# 시스템의 배포판(distribution) 정보를 확인하기 위해 
+# /etc/ 디렉토리의 모든 *release 파일을 출력
+# (예: /etc/os-release, /etc/lsb-release 등)
+
 cat /etc/*release
