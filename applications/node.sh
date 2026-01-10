ca7400123a436f61d4e55fe8689005b0fc3b9438
gyuha <nicegyuha@gmail.com>
2026-01-11 00:58:17 +0900

    update

--- applications/node.sh
+++ applications/node.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# install Volta
-curl https://get.volta.sh | bash
+# install mise
+# curl https://get.volta.sh | bash
+curl https://mise.run/zsh | sh
+
+echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc1
+echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
 
 # install Node
-volta install node
+mise use --global node@24
+mise use --global bun
 
 # start using Node
 node
