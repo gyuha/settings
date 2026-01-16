9d6f24d1764ca700d45d237b0e7b355b2a422a49
gyuha <nicegyuha@gmail.com>
2026-01-16 20:53:33 +0900

    update

---
+++ applications/task.sh
@@ -0,0 +1,10 @@
+# Task
+#		The Modern Task Runner
+#		A fast, cross-platform build tool inspired by Make, designed for modern workflows.
+
+# If you Set up the repository by running:
+
+curl -1sLf 'https://dl.cloudsmith.io/public/task/task/setup.deb.sh' | sudo -E bash
+
+#Then you can install Task with:
+apt install task
